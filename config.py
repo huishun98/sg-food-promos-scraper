65d0eef765d825a3a5017cf7f9936eb043011fd7
huishun98 <huishun98@gmail.com>
2023-06-14 14:46:48 +0800

    First commit

---
+++ config.py
@@ -0,0 +1,34 @@
+import json, base64
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+""" Telegram client config """
+# For more info on how to obtain these values, refer to
+# https://shallowdepth.online/posts/2021/12/end-to-end-tests-for-telegram-bots/
+# https://blog.1a23.com/2020/03/06/how-to-write-integration-tests-for-a-telegram-bot/
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+# with TelegramClient(sessions.StringSession(), API_ID, API_HASH) as client:
+#     print("Session string:", client.session.save())
+TELETHON_SESSION = getenv("TELETHON_SESSION")
+
+""" GSheets config """
+# Create the Google Sheet manually. Set all values as plain text format.
+# Remember to share the Google Sheet with SERVICE_ACCOUNT_INFO_CLIENT_EMAIL
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
+GSHEET_ID = getenv("GSHEET_ID")  # can be found in the gsheet's url
+OVERVIEW_SHEETNAME = "Sheet1"  # defaults to Sheet1.
+DATA_SHEETNAME = "Sheet2"  # defaults to Sheet2.
+
+# To use Google Sheets API,
+# create a google cloud project: https://developers.google.com/workspace/guides/create-project
+# create service account credentials: https://developers.google.com/workspace/guides/create-credentials#service-account
+SERVICE_ACCOUNT_INFO_B64 = getenv("SERVICE_ACCOUNT_INFO_B64")
+SERVICE_ACCOUNT_INFO = base64.b64decode(SERVICE_ACCOUNT_INFO_B64).decode("utf-8")
+SERVICE_ACCOUNT_INFO = json.loads(SERVICE_ACCOUNT_INFO)
---
+++ main.py
@@ -0,0 +1,115 @@
+import pytz
+import config
+import pygsheets
+import pandas as pd
+from google.oauth2 import service_account
+from datetime import datetime
+from telethon import sessions
+from telethon.sync import TelegramClient
+
+from telethon.tl.functions.messages import GetHistoryRequest
+
+
+""" GSheets """
+creds = service_account.Credentials.from_service_account_info(
+    config.SERVICE_ACCOUNT_INFO, scopes=config.SCOPES
+)
+gc = pygsheets.authorize(custom_credentials=creds)
+gsheet = gc.open_by_key(config.GSHEET_ID)
+overview_sheet = gsheet.worksheet_by_title(config.OVERVIEW_SHEETNAME)
+data_sheet = gsheet.worksheet_by_title(config.DATA_SHEETNAME)
+overview_df = overview_sheet.get_as_df()
+columns = [
+    "date",
+    "channel_username",
+    "id",
+    "message",
+    "_",
+    "peer_id",
+    "out",
+    "mentioned",
+    "media_unread",
+    "silent",
+    "post",
+    "from_scheduled",
+    "legacy",
+    "edit_hide",
+    "pinned",
+    "noforwards",
+    "from_id",
+    "fwd_from",
+    "via_bot_id",
+    "reply_to",
+    "media",
+    "reply_markup",
+    "entities",
+    "views",
+    "forwards",
+    "replies",
+    "edit_date",
+    "post_author",
+    "grouped_id",
+    "reactions",
+    "restriction_reason",
+    "ttl_period",
+]
+
+""" Telegram client """
+session = sessions.StringSession(config.TELETHON_SESSION)
+client = TelegramClient(session, config.API_ID, config.API_HASH)
+client.connect()  # Connect to the server
+client.get_me()  # Issue a high level command to start receiving message
+
+posts = []
+
+# Get the current datetime in UTC
+for i, channel_username in enumerate(overview_df["channel"]):
+    print(f"[INFO] processing {channel_username}...")
+
+    row = overview_df[overview_df["channel"] == channel_username]
+    prev_msg_id = row["latest_msg_id"].iloc[0]
+    try:
+        prev_msg_id = int(prev_msg_id)
+    except Exception:
+        print(f"[INFO] {channel_username} does not have a valid prev_msg_id")
+        continue
+
+    channel_entity = client.get_entity(channel_username)
+    req = GetHistoryRequest(
+        peer=channel_entity,
+        limit=100,
+        offset_date=datetime.now(pytz.utc),
+        offset_id=0,
+        max_id=0,
+        min_id=prev_msg_id + 1,
+        add_offset=0,
+        hash=0,
+    )
+    res = client(req)
+    msgs = res.messages
+
+    print(f"[INFO] {len(msgs)} new messages scraped, channel={channel_username}")
+
+    if len(msgs) <= 0:
+        continue
+
+    for msg in msgs:
+        post = msg.to_dict()
+        post["channel_username"] = channel_username
+        posts.append(post)
+
+    overview_df.loc[i, "latest_msg_id"] = msgs[0].id
+    print(f"[INFO] prev msg id={prev_msg_id}, new msg id={msgs[0].id}, channel={channel_username}")
+
+if len(posts) <= 0:
+    print("[INFO] no new posts")
+    exit()
+
+print("[INFO] updating google sheets...")
+data_sheet.update_row(1, columns)
+df = pd.DataFrame(posts)
+df = df.applymap(str)
+df = df.reindex(columns=columns)
+data_sheet.insert_rows(row=1, values=df.values.tolist())
+overview_sheet.set_dataframe(overview_df, start="A1")
+print("[INFO] success")
